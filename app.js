03ef639ca884d059cc7ce25c3bc2c3b5169f36b9
Adrian Mancuso <adrian.mancuso@gmail.com>
2024-08-01 20:47:52 +0200

    tbs

--- app.js
+++ app.js
@@ -77,23 +77,22 @@ if ( location.protocol != "https:" ) {
   }
 
 function makeDistortionCurve(amount) {
-  var k = typeof amount === 'number' ? amount : 50,
-    n_samples = 44100,
-    curve = new Float32Array(n_samples),
-    deg = Math.PI / 180,
-    i = 0,
-    x;
-  for ( ; i < n_samples; ++i ) {
-    x = i * 2 / n_samples - 1;
-    curve[i] = ( 3 + k ) * x * 20 * deg / ( Math.PI + k * Math.abs(x) );
-  }
-  return curve;
+    var k = typeof amount === 'number' ? amount : 50,
+        n_samples = 44100,
+        curve = new Float32Array(n_samples),
+        deg = Math.PI / 180,
+        i = 0,
+        x;
+    for (; i < n_samples; ++i) {
+        x = i * 2 / n_samples - 1;
+        curve[i] = (3 + k) * x * 20 * deg / (Math.PI + k * Math.abs(x));
+    }
+    return curve;
 };
 
-duck.addEventListener('click', function(event) {
-  osc1.start();
-  osc2.start();
-  osc3.start();
+duck.addEventListener('click', function (event) {
+    permission();
+    startOscillators();
 });
 
 distortion.curve = makeDistortionCurve(800);
